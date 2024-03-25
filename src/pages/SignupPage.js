17e7d9da1f2e9ca9dc2f97ccc7f15e30c7669b7c
Thaw Z1n <103358174+ThawZin-Raymond@users.noreply.github.com>
2024-03-25 23:43:32 +1100

    First Commit

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SignupPage() {
+      return (
+            <div>
+                  <h1>SignupPagen</h1>
+
+            </div>
+      )
+}
+
+export default SignupPage;
